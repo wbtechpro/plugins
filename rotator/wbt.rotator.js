c66d3ce0aa595b9d5354a5968058386304640709
Gleb Kondratenko <gkondratenko@gmail.com>
2013-07-23 23:08:28 +0400

    updated license

--- rotator/wbt.rotator.js
+++ rotator/wbt.rotator.js
@@ -1,8 +1,17 @@
 /**
- * wbt.rotator.js v1.0.0
+ * wbt.rotator.js v1.0.2
  *
  * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * Dependencies: jQuery 1.7+
+ *
+ * Basic usage:
+ * $(".any-selector").wbtRotator({
+ *   frameSrc: "path/template/{{30}}.jpg"
+ * });
+ *
+ * For more instructions and examples, please visit http://wbtech.pro/blog/articles/rotator/
  *
  * Copyright 2012, WBTech
  * http://wbtech.pro/
